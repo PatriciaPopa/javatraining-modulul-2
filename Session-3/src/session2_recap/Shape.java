0c0b1f4a3dd16ef83e0796d16c4640536ad55052
Patricia <patriciapopa1997@gmail.com>
2019-10-16 22:43:38 +0300

    added equals() demo

--- Session-3/src/session2_recap/Shape.java
+++ Session-3/src/session2_recap/Shape.java
@@ -14,4 +14,29 @@ public abstract class Shape {
 
 	public abstract void draw();
 
+	@Override
+	public boolean equals(Object obj) {
+		/*
+		 * Prima data trebuie sa verificam ca obiectul pasat ca parametru este de tip
+		 * Shape. Daca nu, putem de la bun inceput sa returnam false. Daca obiectul este
+		 * un Shape, il castuim la tipul Shape, ca sa putem accesa atributele specifice
+		 * acestuia, si returnam true doar daca fieldul color are aceeasi valoare la
+		 * ambele obiecte. Va reamintesc, pentru a compara 2 stringuri folosim metoda
+		 * equals(), nu operatorul == (vezi Curs 3)
+		 */
+		if (obj instanceof Shape && color.equals(((Shape) obj).getColor())) {
+
+			/*
+			 * Daca nu am pune acest if, chiar daca am compara un cerc si un triunghi, dar
+			 * care au aceeasi culoare, metoda equals ar returna true, ca si cum ele ar fi
+			 * acelasi obiect, ceea ce in lumea reala nu ar avea sens
+			 */
+			if (this.getClass().getName().equals(obj.getClass().getName())) {
+				return true;
+			}
+		}
+
+		return false;
+	};
+
 }
--- Session-3/src/session2_recap/ShapeDemo.java
+++ Session-3/src/session2_recap/ShapeDemo.java
@@ -20,5 +20,46 @@ public class ShapeDemo {
 		 * varianta din clasa Object
 		 */
 
+		// -----------------------------------------------------------------------
+
+		System.out.println("Demo metoda equals():");
+
+		String s1 = "abc";
+		String s2 = s1;
+		/*
+		 * new creaza tot timpul un nou obiect; daca puneam si aici doar "abc", cele 3
+		 * referinte pointau catre acelasi obiect.
+		 */
+		String s3 = new String("abc");
+
+		System.out.println("s1 == s2 => " + (s1 == s2));
+		System.out.println("s1 == s3 => " + (s1 == s3));
+
+		System.out.println("s1.equals(s2) => " + s1.equals(s3));
+		System.out.println("s1.equals(s3) => " + s1.equals(s3));
+
+		Circle sh1 = new Circle("blue");
+		Shape sh2 = sh1;
+		Shape sh3 = new Circle("blue");
+		Shape sh4 = new Triangle("blue");
+
+		/*
+		 * Noi am spus ca daca avem acelasi tip de figura geometrica, si de aceeasi
+		 * culoare, cele doua obiecte se considera identice. Dar == compara daca cele
+		 * doua referinte pointeaza la acelasi obiect in memorie, asa ca nu ne ajuta la
+		 * ce vrem noi. De asta am suprascis metoda equals.
+		 */
+		System.out.println("sh1 == sh2 => " + (sh1 == sh2));
+		System.out.println("sh1 == sh3 => " + (sh1 == sh3));
+
+		System.out.println("sh1.equals(sh2) => " + sh1.equals(sh3));
+		/*
+		 * Observati, de asemenea, ca nu conteaza daca comparam o referinta de tip Shape
+		 * si una de tip Circle, atata timp cat ambele pointeaza la aceeasi forma, de
+		 * aceeasi culoare
+		 */
+		System.out.println("sh1.equals(sh3) => " + sh1.equals(sh3));
+		System.out.println("sh1.equals(sh4) => " + sh1.equals(sh4));
+
 	}
 }
